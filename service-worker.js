61dfad5eadb93037906c5c4a8238ea01cdd53930
KNODRON <juan.jimenezv08@gmail.com>
2025-11-27 15:48:09 -0300

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "migratorio-analisis-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  // Estratégia cache-first para archivos propios
+  if (request.method === "GET" && request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(request).then((cached) => {
+        return cached || fetch(request);
+      })
+    );
+  }
+});
